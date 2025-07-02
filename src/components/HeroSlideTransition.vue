1c4a01d60f8ef8e75f6981f201e09ed187aca21d
Mark Atherton <mark@insightinternet.co.uk>
2025-07-02 19:41:29 +0100

    added food images and fixed control styles

--- src/components/HeroSlideTransition.vue
+++ src/components/HeroSlideTransition.vue
@@ -1,32 +1,35 @@
 <script setup>
 import { ref, onMounted, onBeforeUnmount } from "vue";
-import camera from "../assets/slider-imgs/camera.jpg";
+/* import camera from "../assets/slider-imgs/camera.jpg";
 import iphone from "../assets/slider-imgs/iphone.jpg";
 import mixinDesk from "../assets/slider-imgs/mixing-desk.jpg";
-import mixinDesk2 from "../assets/slider-imgs/mixing-desk2.jpg";
+import mixinDesk2 from "../assets/slider-imgs/mixing-desk2.jpg"; */
 import skateboard from "../assets/slider-imgs/skateboard.jpg";
-
-const images = [camera, iphone, mixinDesk, mixinDesk2, skateboard];
+import burger from "../assets/slider-imgs/burgernchips.jpg";
+import croquettes from "../assets/slider-imgs/croquettes.jpg";
+import nachos from "../assets/slider-imgs/nachos.jpg";
+import pescadito from "../assets/slider-imgs/pescadito-frito.jpg";
+const images = [burger, croquettes, nachos, pescadito, skateboard];
 const slides = [
   {
-    img: camera,
-    title: "Welcome",
-    text: "This is the first slide",
+    img: burger,
+    title: "Spanish Beef burger",
+    text: "onion, lettuce, tomato, Spanish ham, parmesan & asparagus.",
   },
   {
-    img: iphone,
-    title: "Discover",
-    text: "This is the second slide",
+    img: croquettes,
+    title: "Croquettes",
+    text: "various varieties: Mushroom, Truffle, Spanish Ham.",
   },
   {
-    img: mixinDesk,
-    title: "Enjoy",
-    text: "This is the third slide",
+    img: nachos,
+    title: "Nachos",
+    text: "Nachos with fresh chilli, cheese, tomatos, onions & guacamole.",
   },
   {
-    img: mixinDesk2,
-    title: "Explore",
-    text: "This is the fourth slide",
+    img: pescadito,
+    title: "Pescadito frito",
+    text: "deep fried crispy fish & fries with lemon and garlic mayonnaise.",
   },
   {
     img: skateboard,
@@ -98,21 +101,49 @@ onBeforeUnmount(() => {
     <!-- Controls -->
     <button
       @click="prevAndReset"
-      class="btn btn-white absolute left-4 top-1/2 -translate-y-1/2"
+      class="btn btn-ghost absolute left-4 top-1/2 transform z-10"
       aria-label="Previous slide"
       title="Previous slide"
       alt="previous slide control"
     >
-      <
+      <!-- SVG for previous arrow -->
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="#d4a750"
+        class="w-6 h-6"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M15 19l-7-7 7-7"
+        />
+      </svg>
     </button>
     <button
       @click="nextAndReset"
-      class="btn btn-white absolute right-4 top-1/2 -translate-y-1/2"
+      class="btn btn-ghost absolute right-4 top-1/2 transform z-10"
       aria-label="Next slide"
       title="Next slide"
       alt="next slide control"
     >
+      <!-- SVG for next arrow -->
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="#d4a750"
+        class="w-6 h-6"
       >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M9 5l7 7-7 7"
+        />
+      </svg>
     </button>
   </div>
 </template>
