6398eeab9cee4ee082a9e2b2ef62828669911d86
Mark Atherton <mark@insightinternet.co.uk>
2025-07-08 13:57:23 +0100

    created test NYCmap.vue and added custom Marker on GoogleMap.vue

--- src/components/NYCmap.vue
+++ src/components/NYCmap.vue
@@ -1,22 +1,19 @@
 <script setup>
-import { GoogleMap, CustomMarker, Marker } from "vue3-google-map";
+import { GoogleMap, CustomMarker } from "vue3-google-map";
 
-const center = { lat: 55.960047, lng: -3.171365 };
-const apiKey = "AIzaSyBLgcpsxfGInIpfUDO1oPY7Rp3k66jFoto"; // define apiKey here
+const center = { lat: 52.36834, lng: 4.88635 };
 </script>
 
 <template>
-  <div class="divider">Location</div>
   <GoogleMap
-    :api-key="apiKey"
+    api-key="YOUR_GOOGLE_MAPS_API_KEY"
     style="width: 100%; height: 500px"
     :center="center"
-    :zoom="19"
+    :zoom="15"
   >
     <CustomMarker :options="{ position: center, anchorPoint: 'BOTTOM_CENTER' }">
       <div
         style="
-          text-align: center;
           background: #192b39;
           border-radius: 8px;
           padding: 10px;
@@ -28,9 +25,9 @@ const apiKey = "AIzaSyBLgcpsxfGInIpfUDO1oPY7Rp3k66jFoto"; // define apiKey here
         >
           <img
             src="../assets/mash-tun-icon.jpg"
-            width="20"
-            height="20"
-            style="display: relative; float: left; margin-right: 0.25rem"
+            width="50"
+            height="50"
+            style="margin-left: 30px"
           />
           The Mash Tun
         </div>
@@ -40,7 +37,7 @@ const apiKey = "AIzaSyBLgcpsxfGInIpfUDO1oPY7Rp3k66jFoto"; // define apiKey here
 </template>
 <script>
 export default {
-  name: "GoogleMap",
+  name: "NYCmap",
 };
 </script>
 <style lang=""></style>
