7944b92c1ce9d1ef75c76db1038caeecb4d82baa
Mark Atherton <mark@insightinternet.co.uk>
2025-07-08 12:56:24 +0100

    created basic Vue 3 map component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import navHeader from "./components/navHeader.vue";
 import HeroSlider from "./components/HeroSlideTransition.vue";
+import GoogleMap from "./components/GoogleMap.vue";
 </script>
 
 <template>
   <navHeader />
   <HeroSlider />
+  <GoogleMap />
 </template>
 
 <style scoped></style>
---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { GoogleMap, Marker } from "vue3-google-map";
+
+const center = { lat: 55.960047, lng: -3.171365 };
+const apiKey = "AIzaSyBLgcpsxfGInIpfUDO1oPY7Rp3k66jFoto"; // define apiKey here
+</script>
+
+<template>
+  <GoogleMap
+    :api-key="apiKey"
+    style="width: 100%; height: 500px"
+    :center="center"
+    :zoom="15"
+  >
+    <Marker :options="{ position: center }" />
+  </GoogleMap>
+</template>
+<script>
+export default {
+  name: "GoogleMap",
+};
+</script>
+<style lang=""></style>
