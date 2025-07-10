ef691ffbb29ef2cb157464237ecaf99886670145
Mark Atherton <mark@insightinternet.co.uk>
2025-07-10 14:45:29 +0100

    created a quick promotions section with images I extracted from mockup

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <script setup>
 import navHeader from "./components/navHeader.vue";
 import HeroSlider from "./components/HeroSlideTransition.vue";
+import Promotion from "./components/Promotion.vue";
 import Contact from "./components/Contact.vue";
 </script>
 
 <template>
   <navHeader />
   <HeroSlider />
+  <Promotion />
+
   <Contact />
 </template>
 
---
+++ src/components/Promotion.vue
@@ -0,0 +1,42 @@
+<script setup>
+import cockTailsImg from "../assets/cocktails.jpg";
+import drinkImg from "../assets/drink.png";
+</script>
+
+<template>
+  <div class="divider">Promotions</div>
+  <div
+    class="grid grid-cols-1 md:grid-cols-2 gap-4 justify-items-center bg-gray-100 p-4"
+  >
+    <img
+      :src="cockTailsImg"
+      alt="Cocktails at The Mash Tun"
+      class="h-64 w-full max-w-xs object-cover rounded mx-auto"
+    />
+    <img
+      :src="drinkImg"
+      alt="Drinks at The Mash Tun"
+      class="h-64 w-full max-w-xs object-cover rounded mx-auto"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Promotion",
+  components: { cockTailsImg, drinkImg },
+  data() {
+    return {
+      // Define any data properties needed for the Promotion component
+    };
+  },
+  methods: {
+    // Define any methods needed for the Promotion component
+  },
+  mounted() {
+    // Any initialization logic can go here
+  },
+};
+</script>
+
+<style lang=""></style>
