8340b6e6dc60698fad644408b3aaa7ea6f501ef3
Mark Atherton <mark@insightinternet.co.uk>
2025-07-10 15:52:39 +0100

    added bg-navbar class and SVGs to finish footer layout

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@ import navHeader from "./components/navHeader.vue";
 import HeroSlider from "./components/HeroSlideTransition.vue";
 import Promotion from "./components/Promotion.vue";
 import Contact from "./components/Contact.vue";
+import Footer from "./components/Footer.vue";
+
 import cockTailsImg from "./assets/cocktails.jpg";
 import drinkImg from "./assets/drink.png";
 </script>
@@ -11,8 +13,8 @@ import drinkImg from "./assets/drink.png";
   <navHeader />
   <HeroSlider />
   <Promotion :imgSrc="cockTailsImg" :imgSrc2="drinkImg" />
-
   <Contact />
+  <Footer />
 </template>
 
 <style scoped></style>
