b49954224cb28b38d86224d8e4b99a9a442aa259
Mark Atherton <mark@insightinternet.co.uk>
2025-07-10 14:55:17 +0100

    Started making Promotion dynamic by pasisng in img src path as prop from App

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@ import navHeader from "./components/navHeader.vue";
 import HeroSlider from "./components/HeroSlideTransition.vue";
 import Promotion from "./components/Promotion.vue";
 import Contact from "./components/Contact.vue";
+import cockTailsImg from "./assets/cocktails.jpg";
+import drinkImg from "./assets/drink.png";
 </script>
 
 <template>
   <navHeader />
   <HeroSlider />
-  <Promotion />
+  <Promotion :imgSrc="cockTailsImg" :imgSrc2="drinkImg" />
 
   <Contact />
 </template>
--- src/components/Promotion.vue
+++ src/components/Promotion.vue
@@ -1,20 +1,22 @@
 <script setup>
-import cockTailsImg from "../assets/cocktails.jpg";
-import drinkImg from "../assets/drink.png";
+import { defineProps } from "vue";
+
+const props = defineProps({
+  imgSrc: String,
+  imgSrc2: String,
+});
 </script>
 
 <template>
   <div class="divider">Promotions</div>
-  <div
-    class="grid grid-cols-1 md:grid-cols-2 gap-4 justify-items-center bg-gray-100 p-4"
-  >
+  <div class="grid grid-cols-1 md:grid-cols-2 gap-4 justify-items-center bg-gray-100 p-4">
     <img
-      :src="cockTailsImg"
+      :src="imgSrc"
       alt="Cocktails at The Mash Tun"
       class="h-64 w-full max-w-xs object-cover rounded mx-auto"
     />
     <img
-      :src="drinkImg"
+      :src="imgSrc2"
       alt="Drinks at The Mash Tun"
       class="h-64 w-full max-w-xs object-cover rounded mx-auto"
     />
@@ -24,7 +26,6 @@ import drinkImg from "../assets/drink.png";
 <script>
 export default {
   name: "Promotion",
-  components: { cockTailsImg, drinkImg },
   data() {
     return {
       // Define any data properties needed for the Promotion component
