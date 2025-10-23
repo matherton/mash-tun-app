689748bf9e185a6def70b4a09e02c178e75003a6
Mark Atherton <mark@insightinternet.co.uk>
2025-10-23 13:08:30 +0100

    commented out menu link

--- src/components/navHeader.vue
+++ src/components/navHeader.vue
@@ -27,13 +27,13 @@ import logo from "../assets/mash-tun-icon.jpg";
           tabindex="0"
           class="menu menu-sm dropdown-content bg-navbar text-[#d4a750] rounded-box z-1 mt-3 w-52 p-2 shadow"
         >
-          <li>
+          <!--  <li>
             <a
               href="https://drive.google.com/file/d/1SAgxkR3KmCDCVMemOT1pp4GFUvOWQM_J/view"
               target="_blank"
-              >Menu</a
+              >Menu </a
             >
-          </li>
+          </li> -->
           <li><a href="#promotions">Promotions</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
